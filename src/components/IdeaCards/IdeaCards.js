e9976999612a2c3ec12882f609e019ade84352c7
rakshitha <rakshitha@robosoftin.com>
2022-04-19 19:35:17 +0530

    implemented sort feature

--- src/components/IdeaCards/IdeaCards.js
+++ src/components/IdeaCards/IdeaCards.js
@@ -1,10 +1,13 @@
 import { useState, useEffect } from 'react'
 import { getData } from '../../utils/fetchData';
 import Card from '../Card/Card';
+import styles from "./IdeaCards.module.css";
 
 const IdeaCards = ({updated}) => {
     const [data, setData] = useState([]);
     const [updateDataOnVote, setupdateDataOnVote] = useState(0);
+    const TIMESTAMP_CREATED = "TIMESTAMP_CREATED";
+    const UPVOTE_COUNT = "UPVOTE_COUNT";
 
     useEffect(() => {
         const func = async () => {
@@ -28,8 +31,23 @@ const IdeaCards = ({updated}) => {
         setupdateDataOnVote((prevValue) => {return ++prevValue})
     }
 
+    const sortHandler = (e) => {
+        if (e.target.value === TIMESTAMP_CREATED) {
+            setData((prevData) => {return [].concat(prevData.sort((a,b) => (new Date(b.createdAt) - new Date(a.createdAt))))})
+        } else if (e.target.value === UPVOTE_COUNT) {
+            setData((prevData) => {return [].concat(prevData.sort((a, b) => (b.upvote - a.upvote)))})
+        }
+    }
+
     return(
         <>
+            <div className={styles.sortContainer}>
+                <label className={styles.sortLabel}>Sort By: </label>
+                <select size="1" name="sort" className={styles.dropdown} onChange={sortHandler}>
+                    <option value={TIMESTAMP_CREATED}>Idea Created</option>
+                    <option value={UPVOTE_COUNT}>No of Votes</option>
+                </select>
+            </div>
             {data && data.map((idea) => {
                 return <Card key={idea.id} idea={idea} updateVote={updateVote} />
             })}
